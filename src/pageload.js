ecbf6afa1960b8a11c9a8e0ae21ab8c6e5a2f21f
tavo <gasb_150@hotmail.com>
2021-03-23 17:12:59 -0500

    add css loader

--- src/pageload.js
+++ src/pageload.js
@@ -1,5 +1,5 @@
-import restauran from './restaurant.jpg'
-import style from './style.css'
+import restauran from './restaurant.jpg';
+import './style.css';
 const print = () => {
 
     function pageLoad() {
@@ -8,12 +8,18 @@ const print = () => {
             //alert('hashashas')
             let content = document.getElementById('content')
             let title = document.createElement('h1')
-            let paragraph = document.createElement('p')
+            
             title.innerHTML = "RESTAURANT :D"
             content.appendChild(title)
+
+            let paragraph = document.createElement('p')
             paragraph.innerHTML = 'lorem26'
             content.appendChild(paragraph)
-            // alert(content)
+            
+            const images = new Image();
+            images.src = restauran
+            
+            content.appendChild(images);
             // content.innerHTML = 'asasas'
         }); 
     };
